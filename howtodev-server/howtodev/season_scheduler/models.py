63ee20d20cf4fc3580a9fac768435eb9c02cbfab
Warren, Ben <bwarren@eab.com>
2020-04-15 19:42:38 -0400

    Define a model

--- howtodev-server/howtodev/season_scheduler/models.py
+++ howtodev-server/howtodev/season_scheduler/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Season(models.Model):
+
+    display_name = models.CharField(max_length=200)
+    start_dt = models.DateField()
+    start_end = models.DateField()
