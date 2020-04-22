d1822460d178dd25caa01d857e05af502d4b42c2
Warren, Ben <bwarren@eab.com>
2020-04-22 19:57:41 -0400

    Prettify components

--- howtodev-vue/src/App.vue
+++ howtodev-vue/src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <PlannerPage msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import PlannerPage from './pages/PlannerPage.vue'
+import PlannerPage from './pages/PlannerPage.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    PlannerPage,
+  },
+};
 </script>
 
 <style>
