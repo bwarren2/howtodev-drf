4ccb97a9656973b6386f1b60b2532ae76fc6cf97
Warren, Ben <bwarren@eab.com>
2020-04-30 01:19:45 -0400

    Some comments

--- howtodev-server/howtodev/drf_exercise/models.py
+++ howtodev-server/howtodev/drf_exercise/models.py
@@ -1,15 +1,21 @@
+"Some toy models"
+
 from django.db import models
 
 
 class Team(models.Model):
+    "A group of Employees."
     name = models.CharField(max_length=255)
 
 
 class Employee(models.Model):
+    "A person working at a fake company."
     name = models.CharField(max_length=255)
     teams = models.ManyToManyField('Team')
 
 
 class Snack(models.Model):
+    "A consumable owned by an employee."
+
     owner = models.ForeignKey('Employee', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
