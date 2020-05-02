54941c9fc7131262445233bc4d216d2934acb790
Warren, Ben <bwarren@eab.com>
2020-05-02 01:41:33 -0400

    Add tests

--- howtodev-server/howtodev/drf_exercise/apis.py
+++ howtodev-server/howtodev/drf_exercise/apis.py
@@ -1,9 +1,11 @@
 "APIs for the exercise"
 
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions, pagination
 from . import serializers, models
 
 
 class EmployeeModelViewSet(viewsets.ModelViewSet):  # pylint: disable=missing-class-docstring
     queryset = models.Employee.objects.all()
     serializer_class = serializers.EmployeeSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+    pagination_class = pagination.LimitOffsetPagination
--- howtodev-server/tests/py/howtodev/drf_exercise/test_002_api.py
+++ howtodev-server/tests/py/howtodev/drf_exercise/test_002_api.py
@@ -2,6 +2,7 @@
 
 import rest_framework
 import pytest
+from django.contrib.auth.models import User
 from django.urls import reverse
 
 try:
@@ -13,6 +14,11 @@ except ImportError:
 @pytest.fixture()
 def employee(db):
     return models.Employee.objects.create(name='John')
+
+
+@pytest.fixture()
+def user(db):
+    return User.objects.create(username='user', email='user@domain.com', password='password')
 
 
 @pytest.fixture
@@ -71,18 +77,42 @@ class TestAPIWorks():  # pylint: disable=missing-class-docstring
     @pytest.mark.run(order=10)
     @pytest.mark.django_db(transaction=True, reset_sequences=True)
     @pytest.mark.it('can be hit')
-    def test_resolve_request(self, api_client, employee):  # pylint: disable=missing-function-docstring
+    def test_resolve_request(self, user, api_client, employee):  # pylint: disable=missing-function-docstring
+        api_client.force_authenticate(user)
         assert api_client.get('/api/v1/employees/').status_code == 200
 
     @pytest.mark.run(order=11)
     @pytest.mark.django_db(transaction=True, reset_sequences=True)
     @pytest.mark.it('returns a list of employees')
-    def test_resolve_employee_list(self, api_client, employee):  # pylint: disable=missing-function-docstring
+    def test_resolve_employee_list(self, user, api_client, employee):  # pylint: disable=missing-function-docstring
+        api_client.force_authenticate(user)
         models.Employee.objects.create(name='Ben')
-        assert api_client.get(reverse('employee-list')).json() == [{'name': 'John'}, {'name': 'Ben'}]
+        response = api_client.get(reverse('employee-list')).json()
+        expected = [{'name': 'John'}, {'name': 'Ben'}]
+        assert response == expected or response['results'] == expected
 
     @pytest.mark.run(order=12)
     @pytest.mark.django_db(transaction=True, reset_sequences=True)
     @pytest.mark.it('returns a specific employee')
+    def test_resolve_employee_detail(self, user, api_client, employee):  # pylint: disable=missing-function-docstring
+        api_client.force_authenticate(user)
+        response = api_client.get(reverse('employee-detail', args=(1,))).json()
+        expected = {'name': 'John'}
+        assert response == expected or response['results'] == expected
+
+    @pytest.mark.run(order=13)
+    @pytest.mark.django_db(transaction=True, reset_sequences=True)
+    @pytest.mark.it('requires login')
     def test_resolve_employee_detail(self, api_client, employee):  # pylint: disable=missing-function-docstring
-        assert api_client.get(reverse('employee-detail', args=(1,))).json() == {'name': 'John'}
+        assert api_client.get(reverse('employee-detail', args=(1,))).status_code == 403
+
+    @pytest.mark.run(order=14)
+    @pytest.mark.django_db(transaction=True, reset_sequences=True)
+    @pytest.mark.it('supports limit-offset pagination with page size 2')
+    def test_employee_pagination(self, user, api_client):  # pylint: disable=missing-function-docstring
+        models.Employee.objects.create(name='Ben')
+        models.Employee.objects.create(name='Jason')
+        models.Employee.objects.create(name='John')
+        api_client.force_authenticate(user)
+        response = api_client.get(reverse('employee-list')).json()
+        assert 'count' in response and 'next' in response and 'previous' in response
